e5d79a0019d6b20f2db3b44aab06ebd867d04b38
Lucas Ramson Siefert <lucasramson@gmail.com>
2025-12-04 14:28:47 -0300

    feat: added double quotting

--- src/utils.rs
+++ src/utils.rs
@@ -2,14 +2,18 @@ pub fn parse_input(input: &str) -> Vec<String> {
     let mut args: Vec<String> = Vec::new();
     let mut current_arg: String = String::new();
     let mut in_quotes:bool = false;
+    let mut in_double_quotes: bool = false;
 
     for c in input.chars() {
         match c {
             '\'' => {
                 in_quotes = !in_quotes;
             },
+            '"' => {
+                in_double_quotes = !in_double_quotes;
+            },
             c if c.is_whitespace()=> {
-                if in_quotes {
+                if in_quotes || in_double_quotes {
                     current_arg.push(c);
                 } else if !current_arg.is_empty() {
                     args.push(current_arg);
