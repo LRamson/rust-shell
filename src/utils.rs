28bb5665a3a3556596d86ddba26cf2f293eeaecd
Lucas Ramson Siefert <lucasramson@gmail.com>
2025-12-04 21:06:50 -0300

    feat: added escaping with \

--- src/utils.rs
+++ src/utils.rs
@@ -3,21 +3,31 @@ pub fn parse_input(input: &str) -> Vec<String> {
     let mut current_arg: String = String::new();
     let mut in_quotes:bool = false;
     let mut in_double_quotes: bool = false;
+    let mut escape_next: bool = false;
 
     for c in input.chars() {
         match c {
-            '\'' => {
-                if !in_double_quotes {
-                    in_quotes = !in_quotes;
+            '\\' => {
+                if !in_double_quotes && !in_quotes {
+                    escape_next = true;
                 } else {
                     current_arg.push(c);
                 }
+            }
+            '\'' => {
+                if !in_double_quotes && !escape_next {
+                    in_quotes = !in_quotes;
+                } else {
+                    current_arg.push(c);
+                    escape_next = false;
+                }
             },
             '"' => {
-                if !in_quotes {
+                if !in_quotes && !escape_next {
                     in_double_quotes = !in_double_quotes;
                 } else {
                     current_arg.push(c);
+                    escape_next = false;
                 }
             },
             c if c.is_whitespace()=> {
