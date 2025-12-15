296f48e30df9571991e4c7c802014cc185b9abb5
Lucas Ramson Siefert <lucasramson@gmail.com>
2025-12-15 20:14:54 -0300

    feat: added history writing to HISTFILE on exit

--- src/commands/exit.rs
+++ src/commands/exit.rs
@@ -1,11 +1,15 @@
-use std::io::Write;
+use std::{env, io::Write};
 
 use super::{{Command, ShellStatus, CommandRegistry}};
 
 pub struct ExitCommand;
 
 impl Command for ExitCommand {
-    fn execute(&self, _: &[String], _: &CommandRegistry, _: &mut dyn Write) -> Result<ShellStatus, String> {
+    fn execute(&self, _: &[String], registry: &CommandRegistry, _: &mut dyn Write) -> Result<ShellStatus, String> {
+        let path_hist = env::var("HISTFILE").unwrap_or_default();
+        if path_hist != "" {
+            let _ = registry.write_history_to_file(&path_hist, true);
+        }
         Ok(ShellStatus::Exit)
     }
 
